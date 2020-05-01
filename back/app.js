89d16bf02b13c132cb2a57ef1afada83215b38c4
kshitijk83 <kshitijk83@gmail.com>
2020-05-01 17:04:32 +0530

    adding a route

--- back/app.js
+++ back/app.js
@@ -2,6 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', function (req, res, next) {
+    res.status(200).json({
+        data: 'hey',
+    })
+})
+
 app.listen(process.env.SERVER_PORT | 4000, () => {
     console.log(`server started on port ${process.env.SERVER_PORT}`)
 })
