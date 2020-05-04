7d1419142c878dca369b1519f1f075deee06af3a
kshitijk83 <kshitijk83@gmail.com>
2020-05-04 09:53:47 +0530

    minor

--- front/src/components/Demo.tsx
+++ front/src/components/Demo.tsx
@@ -1,7 +1,7 @@
-import React, { FC } from 'react'
+import React, { FC } from 'react';
 
 const Demo: FC = (props) => {
-    return <div>asdfasfd</div>
-}
+    return <div>asdfasfd</div>;
+};
 
-export default Demo
+export default Demo;
