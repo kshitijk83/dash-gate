e78af1c8a2e6b35418c10d6313f69f4319e9cde7
kshitijk83 <kshitijk83@gmail.com>
2020-05-26 13:27:36 +0530

    add submenu for all, out and in students

--- front/src/constants/appConstant.ts
+++ front/src/constants/appConstant.ts
@@ -5,8 +5,25 @@ import {
 } from '@ant-design/icons';
 
 export const navBar = [
-    { title: 'StudentList', to: '/', icon: UserOutlined },
-    { title: 'Navbar 2', to: '/something', icon: VideoCameraOutlined },
+    {
+        title: 'Student List',
+        submenu: [
+            {
+                title: 'All',
+                to: '/',
+            },
+            {
+                title: 'Out of Campus',
+                to: '/out',
+            },
+            {
+                title: 'In Campus',
+                to: '/in',
+            },
+        ],
+        icon: UserOutlined,
+    },
+    { title: 'Add New User', to: '/add', icon: VideoCameraOutlined },
     { title: 'Navbar 3', to: '/something', icon: UploadOutlined },
     { title: 'Navbar 4', to: '/something', icon: UserOutlined },
     { title: 'Navbar 5', to: '/something', icon: UserOutlined },
--- front/src/containers/MainLayout/MainLayout.tsx
+++ front/src/containers/MainLayout/MainLayout.tsx
@@ -4,6 +4,7 @@ import { Layout, Menu } from 'antd';
 import { css } from '@emotion/core';
 import * as appConstant from '../../constants/appConstant';
 import './MainLayout.css';
+import SubMenu from 'antd/lib/menu/SubMenu';
 // css for adjusting
 const siderStyle = css`
     overflow: auto;
@@ -27,15 +28,36 @@ const MainLayout: FC = (props) => {
         setCollapsed((prev) => !prev);
     }
 
-    const menuItems = appConstant.navBar.map((item, i) => (
-        <Menu.Item
-            onClick={() => navigate(item.to)}
-            key={i + 1}
-            icon={<item.icon />}
-        >
-            {item.title}
-        </Menu.Item>
-    ));
+    const menuItems = appConstant.navBar.map((item) => {
+        if (item.submenu) {
+            return (
+                <SubMenu
+                    key={item.title}
+                    icon={<item.icon />}
+                    title={item.title}
+                >
+                    {item.submenu.map((subMenuItem) => (
+                        <Menu.Item
+                            key={subMenuItem.title}
+                            onClick={() => navigate(subMenuItem.to)}
+                        >
+                            {subMenuItem.title}
+                        </Menu.Item>
+                    ))}
+                </SubMenu>
+            );
+        } else {
+            return (
+                <Menu.Item
+                    onClick={() => navigate(item.to)}
+                    key={item.title}
+                    icon={<item.icon />}
+                >
+                    {item.title}
+                </Menu.Item>
+            );
+        }
+    });
 
     return (
         <Layout>
